b806be948e35b935ba9c4d30f869a25b11ea30b9
root <adeboyeakingbelure18@yahoo.com>
2023-01-06 03:23:11 +0000

    interview

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+alx interview questions
+"""
+
+
+def canUnlockAll(boxes):
+    """ method that determines if all the boxes can be opened """
+    if (type(boxes)) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+
+    for x in range(1, len(boxes) - 1):
+        checked_boxes = False
+        for idx in range(len(boxes)):
+            checked_boxes = x in boxes[idx] and k != idx
+            if checked_boxes:
+                break
+        if checked_boxes is False:
+            return checked_boxes
+    return True
